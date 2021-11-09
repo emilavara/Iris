5728f343c0337431c8b3bd4ea76c12a2ee23482a
Emil Avara <emilavara@hotmail.com>
2021-11-09 15:50:04 +0100

    asdw

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let mainWindow;
 
 app.on('ready', function(){
     //create new window
-    mainWindow = new BrowserWindow({minWidth: 380, width: 380, minHeight: 720, height: 720, transparent: true, frame: false });
+    mainWindow = new BrowserWindow({minWidth: 385, width: 385, minHeight: 1200, height: 1200, transparent: true, frame: false });
     //load html into window
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWindow.html'),
